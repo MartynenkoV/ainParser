c0ac69eec6865116dbee3a97d717c3ae1f5fe0dd
Vova <renarblackfox@gmail.com>
2017-03-02 20:57:34 +0200

    complete

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var News = require('../model/news');
 
+var request = require("request");
+var cheerio = require("cheerio");
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -9,7 +13,36 @@ router.get('/', function (req, res, next) {
   });
 });
 
+
+function parse() {
+  request({
+    uri: "https://ain.ua/",
+  }, function (error, response, body) {
+    var $ = cheerio.load(body);
+
+    $(".new_post_item h2").each(function () {
+      var link = $(this);
+      var text = link.text();
+
+      News.findOne({title:text}).exec(function (err, news) {
+        if (!news) {
+          var newItem = new News({
+            title: text
+          });
+          newItem.save();
+        }  
+      });
+
+      console.log(text);
+    });
+  });
+}
+
+
 router.get('/parseNews', function (req, res, next) {
+  parse();
+
+  
   res.render('index', {
     title: 'parse'
   });
